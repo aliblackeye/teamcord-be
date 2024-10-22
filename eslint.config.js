58ac8adeb8f4481ad5669840e6d3d335addd1366
aliblackeye <ali.blackeye@hotmail.com>
2024-10-22 21:44:04 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
-const globals = require("globals");
 const pluginJs = require("@eslint/js");
 
 module.exports = [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
 ];
