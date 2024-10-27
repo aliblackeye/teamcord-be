ce85e4eafb4130405740dc3fe32ce928ca62e8a8
aliblackeye <ali.blackeye@hotmail.com>
2024-10-28 02:46:36 +0300

    last

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,33 @@
 import { createServer } from "node:http";
+import express from "express";
 import { Server } from "socket.io";
+import cors from "cors";
 import { onJoinVoiceChannel } from "./socket-events/on-join-voice-channel";
 import { onSubscribeChannel } from "./socket-events/on-subscribe-channel";
 
 import { onGetVoiceChannel } from "./socket-events/on-get-voice-channel";
 import { onLeaveVoiceChannel } from "./socket-events/on-leave-voice-channel";
 import { Channel, VoiceChannel } from "./types";
+import dotenv from "dotenv";
+dotenv.config();
+
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 export const voiceChannels: VoiceChannel[] = [];
 export let channels: Channel[] = [];
-const httpServer = createServer();
-export const io = new Server(httpServer);
+
+const app = express();
+app.use(cors());
+
+const httpServer = createServer(app);
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
 httpServer
   .once("error", (err) => {
